ba8b9a010b5804c9136ad1caa7502e374e42e42b
Bo_Ryong <152785122+kwonboryong@users.noreply.github.com>
2024-03-16 22:36:33 +0900

    한입 크기로 잘라먹는 리액트
    한입 크기로 잘라먹는 리액트

---
+++ React/OneBite/study/components/Button.jsx
@@ -0,0 +1,18 @@
+
+const Button = ({text, color, children}) => {
+    return (
+    <button style={{color: color}}>
+        { text }
+        { children }
+    </button>
+    )
+};
+
+Button.defaultProps = {
+    color: "black"
+};
+
+
+
+
+export default Button;
